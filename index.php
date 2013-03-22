ee61308912ebc90832399b7060d4c9e7f19eb91e
reecek <reecekol@gmail.com>
2013-03-22 10:09:50 -0400

    initial page setup

--- index.php
+++ index.php
@@ -2,8 +2,44 @@
 <html lang="en">
   <head>
     <title>Time Tracker </title>
+	
+	<style> 
+	  #logo {
+	   font-size:32px;
+	   color:4433F5;
+	  }
+	  
+	  #container{
+	   width:960px;
+	   margin:0 auto;
+	  }
+	  
+	  #project {
+	   padding-top:50px;
+	  
+	  }
+	  
+	</style>
   </head>
-<body>
- <div id="logo"> </div>
-
+<body id="container">
+ <div id="logo">Time Tracker</div>
+  
+ <?php
+   if(0){
+  echo "   
+  <table>
+  
+  </table>
+  ";
+  }
+  else 
+  {
+  
+    echo "<div><p>No New Projects</p></div>";
+  }
+  ?>
+  <div id="project">
+      <label>Project Name</label><input type="text"/><label>Est Hrs</label><input type="text"/><label>Total Hrs</label><input type="text"/>
+	 <button>Add New Project</button>
+  <div>
 </body>
