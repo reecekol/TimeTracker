07d8aa876efc053af32179d57bc1a8e599b75638
reecek <reecekol@gmail.com>
2013-03-22 09:57:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Time Tracker
+
+A small app I built to keep track of my projects
---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype>
+<html lang="en">
+  <head>
+    <title>Time Tracker </title>
+  </head>
+<body>
+ <div id="logo"> </div>
+
+</body>
